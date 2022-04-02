c28b173c40e76c9d14a1ac3b4388c5236b86192d
xiongbojun <1800012832@pku.edu.cn>
2022-04-02 16:23:01 +0800

    version 0.3

---
+++ draw_svg_outlines.sh
@@ -0,0 +1,4 @@
+for char in {A..Z}
+do
+    python3 draw_svg_outlines.py --char_class $char
+done
--- main.py
+++ main.py
@@ -12,6 +12,11 @@ import re
 import os
 from IPython.display import clear_output
 
+def showImage(img):
+    plt.figure(figsize=(10,10))
+    plt.imshow(img, cmap="gray")
+    plt.show()
+
 class SVG:
     def __init__(self,svg_seq):
         self.svg_seq = svg_seq
@@ -31,6 +36,12 @@ def util_sample_from_img(img):
 class Editer:
     def __init__(self,img_path,svg_path, pop_size, seed = 0):
         self.img_grey = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        gx = cv2.Sobel(self.img_grey, cv2.CV_32F, 1, 0, ksize=1)
+        gy = cv2.Sobel(self.img_grey, cv2.CV_32F, 0, 1, ksize=1)
+        mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+        # mag = cv2.bitwise_not(mag)
+        mag = 255 - mag
+        self.img_outline = mag.astype(np.uint8)
         self.svg_path = svg_path
         self.cached_image = None
         self.cached_error = None
@@ -42,7 +53,6 @@ class Editer:
         self.height,self.width = self.img_grey.shape
         self.seed = seed
         self.inital_length = None
-
 
     def InitPopulation(self):
         fin = open(self.svg_path,'r')
@@ -63,7 +73,26 @@ class Editer:
                 command.append(True)
                 command.append('modify')
                 svg_seq.append(command)
+        print(len(svg_seq))
+        if not __debug__:
+            init_img = self.DrawSeq(svg_seq)
+            plt.figure()
+            plt.subplot(1,2,1)
+            plt.imshow(init_img, cmap='gray')
+        svg_seq = self.InitDeleteCommand(svg_seq)
+        print(len(svg_seq))
+        if not __debug__:
+            after_img = self.DrawSeq(svg_seq)
+            plt.subplot(1,2,2)
+            plt.imshow(after_img, cmap='gray')
         self.inital_length = len(svg_seq)
+        if not __debug__:
+            plt.figure()
+            plt.subplot(1,2,1)
+            plt.imshow(self.DrawSeqOutline(svg_seq), cmap='gray')
+            plt.subplot(1,2,2)
+            plt.imshow(self.img_outline, cmap='gray')
+            plt.show()
         for i in range(self.pop_size):
             svg = SVG(copy.deepcopy(svg_seq))
             self.population[self.cur].append(svg)
@@ -78,6 +107,22 @@ class Editer:
                 svg_str += ' '
         svg_data = '<?xml version="1.0" ?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256"><defs/><g>'
         svg_data += '<path d="%s" stroke-width="1.0" fill="rgb(0, 0, 0)" opacity="1.0"/></g></svg>'%svg_str
+        png = svg2png(bytestring=svg_data)
+        pil_img = Image.open(BytesIO(png))
+        render_img = np.array(pil_img)
+        render_img = render_img[:,:,-1]
+        render_img = cv2.bitwise_not(render_img)
+        return render_img
+
+    def DrawOutline(self, svg):
+        svg_seq = svg.svg_seq
+        svg_str = ''
+        for i in range(len(svg_seq)):
+            for j in svg_seq[i][:-2]:
+                svg_str += str(j)
+                svg_str += ' '
+        svg_data = '<?xml version="1.0" ?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256"><defs/><g>'
+        svg_data += '<path d="%s" fill="none" stroke="black" stroke-width="2.0"/></g></svg>'%svg_str
         png = svg2png(bytestring=svg_data)
         pil_img = Image.open(BytesIO(png))
         render_img = np.array(pil_img)
@@ -100,19 +145,49 @@ class Editer:
         render_img = cv2.bitwise_not(render_img)
         return render_img
 
-    def Evaluate(self, render_img):
-        diff1 = cv2.subtract(self.img_grey, render_img) #values are too low
-        diff2 = cv2.subtract(render_img,self.img_grey) #values are too high
-        totalDiff = cv2.add(diff1, diff2)
-        totalDiff = np.sum(totalDiff)
-        totalDiff = totalDiff / (self.height * self.width)
+    def DrawSeqOutline(self, svg_seq):
+        svg_str = ''
+        for i in range(len(svg_seq)):
+            for j in svg_seq[i][:-2]:
+                svg_str += str(j)
+                svg_str += ' '
+        svg_data = '<?xml version="1.0" ?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256"><defs/><g>'
+        svg_data += '<path d="%s" fill="none" stroke="black" stroke-width="2.0"/></g></svg>'%svg_str
+        png = svg2png(bytestring=svg_data)
+        pil_img = Image.open(BytesIO(png))
+        render_outline = np.array(pil_img)
+        render_outline = render_outline[:,:,-1]
+        render_outline = cv2.bitwise_not(render_outline)
+        return render_outline
+
+    def Evaluate(self, render_img, render_outline):
+        diff_img_1 = cv2.subtract(self.img_grey, render_img) #values are too low
+        diff_img_2 = cv2.subtract(render_img,self.img_grey) #values are too high
+        totalDiff_img = cv2.add(diff_img_1, diff_img_2)
+        totalDiff_img = np.sum(totalDiff_img)
+        totalDiff_img = totalDiff_img / (self.height * self.width)
+
+        diff_outline_1 = cv2.subtract(self.img_outline, render_outline) #values are too low
+        diff_outline_2 = cv2.subtract(render_outline,self.img_outline) #values are too high
+        totalDiff_outline = cv2.add(diff_outline_1, diff_outline_2)
+        totalDiff_outline = np.sum(totalDiff_outline)
+        totalDiff_outline = totalDiff_outline / (self.height * self.width)
+
+        totalDiff = totalDiff_img + 0.5 * totalDiff_outline
+
         return totalDiff
 
-    def TotalLoss(self, svg_seq):
-        render_img = self.DrawSeq(svg_seq)
-        totalDiff = self.Evaluate(render_img)
-        totalLoss = 0.8 * totalDiff + 0.2 * len(svg_seq)
-        return totalLoss
+    def Deltaloss(self,svg_seq, tmp_seq, k):
+        init_img = self.DrawSeq(svg_seq)
+        cur_img = self.DrawSeq(tmp_seq)
+        diff1 = cv2.subtract(init_img, cur_img) #values are too low
+        diff2 = cv2.subtract(cur_img, init_img) #values are too high
+        imgDiff = cv2.add(diff1, diff2)
+        imgDiff = np.sum(imgDiff)
+        imgDiff = imgDiff / (self.height * self.width)
+        lengthDiff = len(svg_seq) - len(tmp_seq)
+        deltaloss = lengthDiff - k * imgDiff
+        return deltaloss
 
     def EvaluatePopulation(self):
         loss_best = 1e12
@@ -120,7 +195,8 @@ class Editer:
         for i in range(self.pop_size):
             svg = self.population[self.cur][i]
             render_img = self.Draw(svg)
-            svg.loss = self.Evaluate(render_img)
+            render_outline = self.DrawOutline(svg)
+            svg.loss = self.Evaluate(render_img, render_outline)
             if svg.loss < loss_best:
                 loss_best = svg.loss
                 idx_best = i
@@ -146,10 +222,11 @@ class Editer:
         elif parameter >= max_number:
             return max_number
 
-    def MutatePos(self, svg, seed):
+    def MutatePos(self, svg, moving_dis, seed):
         svg_seq = svg.svg_seq
         cache_image = self.Draw(svg)
-        cache_error = self.Evaluate(cache_image)
+        cache_outline = self.DrawOutline(svg)
+        cache_error = self.Evaluate(cache_image, cache_outline)
         for idx in range(len(svg_seq)):
             if svg_seq[idx][-1] == 'fix':
                 continue
@@ -172,14 +249,26 @@ class Editer:
             #mutate selected items
             np.sort(changeIndices)
             for changeIndex in changeIndices:
-                delta = random.uniform(-2,2)
+                delta = random.uniform(-moving_dis, moving_dis)
                 command[changeIndex] += delta
                 command[changeIndex] = self.Clamp(0, 256-1, command[changeIndex])
             tmp_img = self.DrawSeq(tmp_seq)
-            tmp_error = self.Evaluate(tmp_img)
+            tmp_outline = self.DrawSeqOutline(tmp_seq)
+            tmp_error = self.Evaluate(tmp_img, tmp_outline)
             if tmp_error < cache_error:
                 cache_error = tmp_error
                 svg_seq[idx] = command[:]
+
+    def CheckNeedInsert(self, mag, curX, curY):
+        min_x = self.Clamp(0, self.width, curX - 5)
+        max_x = self.Clamp(0, self.width, curX + 5)
+        min_y = self.Clamp(0, self.height, curY - 5)
+        max_y = self.Clamp(0, self.height, curY + 5)
+        for x in range(min_x, max_x):
+            for y in range(min_y, max_y):
+                if mag[y][x] >= 100:
+                    return True
+        return False
 
     def InsertCommand(self, svg, blur_percent):
         svg_seq = svg.svg_seq
@@ -212,19 +301,19 @@ class Editer:
         for i in range(0, len(svg_seq)):
             curX = svg_seq[index][-4]
             curY = svg_seq[index][-3]
-            if mag[int(curY)][int(curX)] > 100:
+            if self.CheckNeedInsert(mag, int(curX), int(curY)):
                 while(True):
-                        posX, posY = util_sample_from_img(mag)
-                        dis = ((posX - curX)**2 + (posY - curY)**2)**0.5
-                        if dis < 10:
-                            break
+                    posX, posY = util_sample_from_img(mag)
+                    dis = ((posX - curX)**2 + (posY - curY)**2)**0.5
+                    if dis < 10:
+                        break
                 if random.random() < 0.5:
                     command = ['L', posX, posY, False, 'modify']
                     svg_seq.insert(index+1, command)
                 else:
                     command = ['C', curX + (posX - curX)/3,curY + (posY - curY)/3, curX + 2*(posX - curX)/3, curY + 2*(posY - curY)/3, posX, posY, False, 'modify']
-                    svg_seq.insert(index+1, command)
-                    if (index+1) != len(svg_seq)-1:
+                    svg_seq.insert(index + 1, command)
+                    if (index + 1) != len(svg_seq)-1:
                         svg_seq[index + 2][-1] = 'involve_modify'
                 index += 1
             elif mag[int(curY)][int(curX)] < 60:
@@ -232,17 +321,61 @@ class Editer:
                     svg_seq[index][-1] = 'fix'
             index += 1
 
-    # def DeleteCommand(self, svg):
-    #     svg_seq = svg.svg_seq
-    #     cache_totalloss = self.TotalLoss(svg_seq)
-    #     for i in range(len(svg_seq)):
-    #         tmp_seq = copy.deepcopy(svg_seq)
-    #         if tmp_seq[i][-1] == False:
-    #             del tmp_seq[i]
-    #             totalloss = self.TotalLoss(tmp_seq)
-    #             if totalloss < cache_totalloss:
-    #                 del svg_seq[i]
-    #                 cache_totalloss = totalloss
+    def InitDeleteCommand(self, svg_seq):
+        return_svg_seq = copy.deepcopy(svg_seq)
+        index = 2
+        for i in range(2, len(return_svg_seq)):
+            tmp_seq = copy.deepcopy(return_svg_seq)
+            delete_command = tmp_seq[index]
+            last_command = tmp_seq[index - 1]
+            # last_last_command = tmp_seq[i-2]
+            curX = delete_command[-4]
+            curY = delete_command[-3]
+            del tmp_seq[index]
+            if last_command[0] == 'L':
+                last_command[1] = curX
+                last_command[2] = curY
+            elif last_command[0] == 'C':
+                last_command[-4] = curX
+                last_command[-3] = curY
+            deltaloss = self.Deltaloss(return_svg_seq, tmp_seq, 5)
+            if deltaloss > 0:
+                return_svg_seq = copy.deepcopy(tmp_seq)
+            else:
+                index += 1
+        return return_svg_seq
+
+    def DeleteCommand(self, svg_seq):
+        return_svg_seq = copy.deepcopy(svg_seq)
+        index = 2
+        for i in range(2, len(return_svg_seq)):
+            tmp_seq = copy.deepcopy(return_svg_seq)
+            delete_command = tmp_seq[index]
+            if delete_command[-2] == True:
+                index += 1
+                continue
+            else:
+                last_command = tmp_seq[index - 1]
+                curX = delete_command[-4]
+                curY = delete_command[-3]
+                del tmp_seq[index]
+                if last_command[0] == 'L':
+                    last_command[1] = curX
+                    last_command[2] = curY
+                elif last_command[0] == 'C':
+                    last_command[-4] = curX
+                    last_command[-3] = curY
+                cur_render_img = self.DrawSeq(return_svg_seq)
+                cur_render_outlines = self.DrawSeqOutline(return_svg_seq)
+                current_loss = self.Evaluate(cur_render_img, cur_render_outlines)
+                del_render_img = self.DrawSeq(tmp_seq)
+                del_render_outlines = self.DrawSeqOutline(tmp_seq)
+                delete_loss = self.Evaluate(del_render_img, del_render_outlines)
+                if delete_loss  <= current_loss + 0.25:
+                    return_svg_seq = copy.deepcopy(tmp_seq)
+                else:
+                    index += 1
+        return return_svg_seq
 
     def ModifyAll(self):
         for i in range(self.pop_size):
@@ -267,7 +400,7 @@ class Editer:
         c = []
         svg_seq1 = svg1.svg_seq
         svg_seq2 = svg2.svg_seq
-        middle_len = self.inital_length // 2
+        middle_len = random.randint(2, self.inital_length - 1)
         if random.random() < 0.5:
             first_seq = svg_seq1
             second_seq = svg_seq2
@@ -311,11 +444,15 @@ class Editer:
             print(self.population[self.cur][p_best].loss)
             print(len(self.population[self.cur][p_best].svg_seq))
             self.ComputeCrossProb(txi, self.population[self.cur][p_worst].loss)
-            txi /= decay
+            moving_dis = 2
+            decay_rate = 0.9
+            if g >= 400:
+                moving_dis = moving_dis * np.power(decay_rate, (g-400) // 10 )
             if g == 380:
                 self.ModifyAll()
 
             if g < 100 or (g > 210 and g < 250) or (g > 350 and g < 400):
+                txi *= decay
                 for i in range(self.pop_size):
                     p1 = self.Upper_bound(self.pp, random.random())
                     p2 = self.Upper_bound(self.pp, random.random())
@@ -339,7 +476,9 @@ class Editer:
                         plt.subplot(1,2,2)
                         plt.imshow(tmp_img, cmap='gray')
                         plt.show()
-                self.MutatePos(self.population[1 - self.cur][i], self.seed + time.time() + g)
+                if g == 200 or g == 350 or g == 380:
+                    self.population[1 - self.cur][i].svg_seq = copy.deepcopy(self.DeleteCommand(self.population[1 - self.cur][i].svg_seq))
+                self.MutatePos(self.population[1 - self.cur][i],moving_dis, self.seed + time.time() + g)
 
             self.cur = 1 - self.cur
             c_best, c_worst = self.EvaluatePopulation()
@@ -366,6 +505,23 @@ class Editer:
         totalDiff = cv2.add(diff1, diff2)
         return final_svg, final_img, totalDiff
 
+def repair_svg(svg):
+    svg_seq = svg.svg_seq
+    startpoint_x = svg_seq[0][1]
+    startpoint_y = svg_seq[0][2]
+    index = 1
+    for i in range(1, len(svg_seq)):
+        if svg_seq[index][0] == 'M':
+            command = ['L', startpoint_x, startpoint_y, True, 'modify']
+            startpoint_x = svg_seq[index][1]
+            startpoint_y = svg_seq[index][2]
+            svg_seq.insert(index, command)
+            index += 2
+        else:
+            index += 1
+    command = ['L', startpoint_x, startpoint_y, True, 'modify']
+    svg_seq.insert(len(svg_seq), command)
+
 def save_svg(svg, target_dir, name):
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
@@ -382,14 +538,32 @@ def save_svg(svg, target_dir, name):
     svg_f.write(svg_data)
     svg_f.close()
 
+def save_svg_outlines(svg, target_outlines_dir, name):
+    if not os.path.exists(target_outlines_dir):
+        os.mkdir(target_outlines_dir)
+    svg_seq = svg.svg_seq
+    svg_str = ''
+    for i in range(len(svg_seq)):
+        for j in svg_seq[i][:-2]:
+            svg_str += str(j)
+            svg_str += ' '
+    svg_data = '<?xml version="1.0" ?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256"><defs/><g>'
+    svg_data += '<path d="%s" fill="none" stroke="black" stroke-width="2.0"/></g></svg>'%svg_str
+    svg_outfile = os.path.join(target_outlines_dir, f"{name}.svg")
+    svg_f = open(svg_outfile, 'w')
+    svg_f.write(svg_data)
+    svg_f.close()
+
 def main():
-    parser = argparse.ArgumentParser(description="LMDB creation")
-    parser.add_argument('--char_class', type=str, default='B')
+    parser = argparse.ArgumentParser(description="svg genetic editing")
+    parser.add_argument('--char_class', type=str, default='A')
     opts = parser.parse_args()
     print(opts.char_class)
-    editer = Editer(f'target_image/{opts.char_class}.png',f'source_svg/{opts.char_class}.svg', 10, seed=time.time())
-    target_dir = 'target_svg'
+    editer = Editer(f'target_image/149/{opts.char_class}.png',f'source_svg/149/{opts.char_class}.svg', 10, seed=time.time())
+    target_dir = 'target_svg/149'
+    target_outlines_dir = 'target_svg_outlines/149'
     svg, img, totalDiff= editer.Edit(400, 20, 0.9, 0.8)
+    repair_svg(svg)
     if not __debug__:
         plt.figure()
         plt.subplot(1, 2, 1)
@@ -398,6 +572,7 @@ def main():
         plt.imshow(totalDiff, cmap = 'gray')
         plt.show()
     save_svg(svg, target_dir, opts.char_class)
+    save_svg_outlines(svg, target_outlines_dir, opts.char_class)
 
 if __name__ == '__main__':
     main()
